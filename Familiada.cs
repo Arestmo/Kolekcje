de7769a38b9eda5ef3638d0cd2be7f7b53c714f3
MateuszT <mateusz@clearmedia.pl>
2020-02-13 17:02:30 +0100

    App v1

---
+++ Familiada.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje
+{
+    class Familiada
+    {
+        public string Question;
+        public string[] Answers;
+
+        public Familiada(string pytanie, string[] odpowiedzi)
+        {
+            this.Question = pytanie;
+            this.Answers = odpowiedzi;
+        }
+
+        public string GetQuestion() 
+        {
+            return this.Question;
+        }
+
+        public bool Check(string TextFromUser)
+        {
+            return Answers.Contains<string>(TextFromUser); 
+        }
+    }
+}
